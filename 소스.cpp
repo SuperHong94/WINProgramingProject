e39145e1793d4a0ec28e2d627de62aa799661f4b
SuperHong94 <50072558+SuperHong94@users.noreply.github.com>
2019-05-27 23:22:24 +0900

    Create 소스.cpp

---
+++ 소스.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+int main()
+{
+	int a=10;
+	int* p = &a;
+
+	std::cout << *p << std::endl;
+	std::cout << "깃허브 사용어캐하냐" << std::endl;
+	std::cout << "tnwjdkdsnkls" << std::endl;
+}
